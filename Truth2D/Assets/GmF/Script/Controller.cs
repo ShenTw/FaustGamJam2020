dc3c3d174fb08146ac53ee320a5cbb88f1863d9c
gmfnasg12 <gpx_mark_farng@hotmail.com>
2020-08-30 13:12:55 +0800

    互動系統

---
+++ Truth2D/Assets/GmF/Script/Controller.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public bool isAuto = false;
+    public List<KeyCode> KeyCodes = new List<KeyCode>();
+    public bool useMouseRightClick = true;
+    public List<string> DoUnitTags = new List<string>();
+
+    public virtual void Awake()
+    {
+        ControllerManager.RegistController(this);
+    }
+
+    public void OnDestroy()
+    {
+        ControllerManager.UnRegistController(this);
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            PlayerEnterControllerCheck();
+        }
+
+        CheckDoUnitTag(other.gameObject);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            PlayerEnterControllerCheck();
+        }
+
+        CheckDoUnitTag(other.gameObject);
+    }
+
+    bool CheckDoUnitTag(GameObject gameObject)
+    {
+        UnitBase unit = UnitManager.GetUnitByGameObject(gameObject);
+        if (unit != null)
+        {
+            foreach (string unitTag in DoUnitTags)
+            {
+                if (unit.UnitTags.Contains(unitTag))
+                {
+                    Do();
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    void PlayerEnterControllerCheck()
+    {
+        if(isAuto || CheckKeys() || (useMouseRightClick && Input.GetMouseButtonDown(0)) )
+        {
+            Do();
+        }
+    }
+
+    bool CheckKeys()
+    {
+        foreach(KeyCode key in KeyCodes)
+        {
+            if (Input.GetKeyDown(key))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public virtual void Do()
+    {
+
+    }
+}
---
+++ Truth2D/Assets/GmF/Script/ShowAnimatorController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowAnimatorController : ShowController
+{
+    public Animator animator = null;
+    public List<string> AnimationNames = new List<string>();
+    int showAnimationNameIndex = 0;
+    public bool DisableHideAnimator = false;
+    public string InitAnimatorName = "";
+    public bool WattingAnimPlayEnd = true;
+
+    public override void Awake()
+    {
+        base.Awake();
+
+        if (animator != null)
+        {
+            if (DisableHideAnimator)
+            {
+                animator.gameObject.SetActive(false);
+            }
+            if (!string.IsNullOrEmpty(InitAnimatorName))
+            {
+                animator.Play(InitAnimatorName);
+            }
+        }
+    }
+
+    public override void Show()
+    {
+        base.Show();
+
+        if(AnimationNames.Count == 0)
+        {
+            return;
+        }
+        if(animator == null)
+        {
+            return;
+        }
+
+        if(WattingAnimPlayEnd && animator.GetCurrentAnimatorStateInfo(0).IsName(AnimationNames[showAnimationNameIndex]))
+        {
+            ///等待目前動畫結束
+            return;
+        }
+
+        animator.gameObject.SetActive(true);
+
+        string showAnimationName = AnimationNames[showAnimationNameIndex];
+        if (!string.IsNullOrEmpty(showAnimationName))
+        {
+            animator.Play(AnimationNames[showAnimationNameIndex]);
+        }
+        showAnimationNameIndex = Mathf.Min(showAnimationNameIndex, Mathf.Max(0, AnimationNames.Count - 1));
+    }
+}
